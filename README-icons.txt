0e50a2dd346ba3afa4006e13a3f90d507bac5ea4
Johan Walles <johan.walles@gmail.com>
2012-08-06 12:31:11 +0200

    Add a logotype + infrastructure.
    Logo is in logo.blend, go to Blender.org and get Blender to modify.
    
    The infrastructure consists of the render-icons.sh script that needs
    to be run after initial checkout to render the logotype graphics.

---
+++ README-icons.txt
@@ -0,0 +1,4 @@
+Run render-icons.sh after initial checkout; it renders icons and Market graphics.
+
+The script requires Blender, get the latest version here:
+http://www.blender.org/download/get-blender/
---
+++ render-icons.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script uses Blender to render logo.blend into different
+# png files.  Get Blender here:
+# http://www.blender.org/download/get-blender/
+
+# Exit on error
+set -e
+
+# This works for OSX
+BLENDER=$(mdfind 'kMDItemFSName = blender.app && kMDItemKind = Program')/Contents/MacOS/blender
+
+echo $(date): Rendering icon...
+$BLENDER -b logo.blend --scene mdpi -o /tmp/mdpi -F PNG -x 1 -f 1
+mv /tmp/mdpi*.png res/drawable/icon.png
+
+echo $(date): Rendering Market graphics...
+$BLENDER -b logo.blend --scene market -o /tmp/market -F PNG -x 1 -f 1
+mv /tmp/market*.png market-icon.png
+
+echo $(date): Done!
