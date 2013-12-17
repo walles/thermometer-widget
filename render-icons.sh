d98a5636287aeaef7d6d7e6a080b833e2253d932
Johan Walles <johan.walles@gmail.com>
2013-12-17 19:39:00 +0100

    Add hi-res icons
    This is required for the Designed for Tablets approval on Google Play.

--- render-icons.sh
+++ render-icons.sh
@@ -10,9 +10,22 @@ set -e
 # This works for OSX
 BLENDER=$(mdfind 'kMDItemFSName = blender.app && kMDItemKind = Program')/Contents/MacOS/blender
 
-echo $(date): Rendering icon...
+echo $(date): Rendering icons...
+mkdir -p src/main/res/drawable-mdpi
 $BLENDER -b logo.blend --scene mdpi -o /tmp/mdpi -F PNG -x 1 -f 1
-mv /tmp/mdpi*.png src/main/res/drawable/icon.png
+mv /tmp/mdpi*.png src/main/res/drawable-mdpi/icon.png
+
+mkdir -p src/main/res/drawable-hdpi
+$BLENDER -b logo.blend --scene hdpi -o /tmp/hdpi -F PNG -x 1 -f 1
+mv /tmp/hdpi*.png src/main/res/drawable-hdpi/icon.png
+
+mkdir -p src/main/res/drawable-xhdpi
+$BLENDER -b logo.blend --scene xhdpi -o /tmp/xhdpi -F PNG -x 1 -f 1
+mv /tmp/xhdpi*.png src/main/res/drawable-xhdpi/icon.png
+
+mkdir -p src/main/res/drawable-xxhdpi
+$BLENDER -b logo.blend --scene xxhdpi -o /tmp/xxhdpi -F PNG -x 1 -f 1
+mv /tmp/xxhdpi*.png src/main/res/drawable-xxhdpi/icon.png
 
 echo $(date): Rendering Market graphics...
 $BLENDER -b logo.blend --scene market -o /tmp/market -F PNG -x 1 -f 1
