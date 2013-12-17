005a33eefe597065b88eafd2178f8e6d4906291c
Johan Walles <johan.walles@gmail.com>
2013-12-17 22:12:49 +0100

    Add screenshots from 7" and 10" tablets
    Also move all graphics resources into their own folder.

--- render-icons.sh
+++ render-icons.sh
@@ -7,28 +7,33 @@
 # Exit on error
 set -e
 
+MYDIR=$(cd $(dirname "$0") ; pwd)
+
 # This works for OSX
 BLENDER=$(mdfind 'kMDItemFSName = blender.app && kMDItemKind = Program')/Contents/MacOS/blender
 
+cd $MYDIR/graphics
+
 echo $(date): Rendering icons...
-mkdir -p src/main/res/drawable-mdpi
+
+mkdir -p $MYDIR/src/main/res/drawable-mdpi
 $BLENDER -b logo.blend --scene mdpi -o /tmp/mdpi -F PNG -x 1 -f 1
-mv /tmp/mdpi*.png src/main/res/drawable-mdpi/icon.png
+mv /tmp/mdpi*.png $MYDIR/src/main/res/drawable-mdpi/icon.png
 
-mkdir -p src/main/res/drawable-hdpi
+mkdir -p $MYDIR/src/main/res/drawable-hdpi
 $BLENDER -b logo.blend --scene hdpi -o /tmp/hdpi -F PNG -x 1 -f 1
-mv /tmp/hdpi*.png src/main/res/drawable-hdpi/icon.png
+mv /tmp/hdpi*.png $MYDIR/src/main/res/drawable-hdpi/icon.png
 
-mkdir -p src/main/res/drawable-xhdpi
+mkdir -p $MYDIR/src/main/res/drawable-xhdpi
 $BLENDER -b logo.blend --scene xhdpi -o /tmp/xhdpi -F PNG -x 1 -f 1
-mv /tmp/xhdpi*.png src/main/res/drawable-xhdpi/icon.png
+mv /tmp/xhdpi*.png $MYDIR/src/main/res/drawable-xhdpi/icon.png
 
-mkdir -p src/main/res/drawable-xxhdpi
+mkdir -p $MYDIR/src/main/res/drawable-xxhdpi
 $BLENDER -b logo.blend --scene xxhdpi -o /tmp/xxhdpi -F PNG -x 1 -f 1
-mv /tmp/xxhdpi*.png src/main/res/drawable-xxhdpi/icon.png
+mv /tmp/xxhdpi*.png $MYDIR/src/main/res/drawable-xxhdpi/icon.png
 
 echo $(date): Rendering Market graphics...
 $BLENDER -b logo.blend --scene market -o /tmp/market -F PNG -x 1 -f 1
-mv /tmp/market*.png market-icon.png
+mv /tmp/market*.png $MYDIR/graphics/market-icon.png
 
 echo $(date): Done!
